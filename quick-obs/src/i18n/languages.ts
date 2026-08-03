3ab7e27462ac75f26481903b542cecb5e51af473
johnfinmann-ctrl <john.finmann@gmail.com>
2026-08-03 21:12:34 +0200

    Delete quick-obs directory

--- quick-obs/src/i18n/languages.ts
+++
@@ -1,7 +0,0 @@
-import type { LanguageCode } from "./types";
-
-export const SUPPORTED_LANGUAGES: { code: LanguageCode; label: string }[] = [
-  { code: "da", label: "Dansk" },
-  { code: "kl", label: "Kalaallisut" },
-  { code: "fo", label: "Foroyskt" },
-];
